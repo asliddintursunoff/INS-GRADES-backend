621251844ce15710d9ea44f4effc95423f97d1c4
asliddin_tursunoff <asliddintursunoff3@gmail.com>
2026-02-26 09:56:59 +0500

    Add update_classes endpoint for CSV updates

--- app/api/routers/classes.py
+++ app/api/routers/classes.py
@@ -11,3 +11,7 @@ router = APIRouter(
 @router.post("/rebase-with-csv")
 async def rebase_subjects(session:class_session,file:UploadFile):
     return await session.adding_classes_by_csv(file)
+
+@router.post("/update-classes-with-csv")
+async def update_classes(session:class_session,file:UploadFile):
+    return await session.update_existing_classtimes_by_csv(file)
